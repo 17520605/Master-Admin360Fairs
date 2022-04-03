44a8d99fe4bc453ebc40ead2d27f11698757f864
17520605 <nguyenhuuminhkhai@gmail.com>
2022-04-03 21:34:03 +0700

    fix model Users to User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
 {
     public function index()
     {   
-        $users = \App\Models\Users::where([
+        $users = \App\Models\User::where([
             ['type','touradmin']
         ])->get();
         foreach($users as $user)
