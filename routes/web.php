45148ba20fd9cd148274f0056d2707e2ef783288
Nguyễn Hữu Minh Khai <nguyenhuuminhkhai@gmail.com>
2022-06-01 23:04:09 +0700

    Upload 6-1-22

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@
         });
         Route::group(['prefix' => 'users'], function(){
             Route::get('/', 'UserController@index')->name('master.get.user.list-users');
+            Route::get('/guest', 'UserController@guest')->name('master.get.user.list-guest');
             Route::get('/create', 'UserController@create')->name('master.get.user.create');
             Route::post('/save-create', 'UserController@saveCreate')->name('master.post.user.save-create');
             Route::get('/{id}/edit/', 'UserController@edit')->name('master.get.user.edit');
@@ -27,10 +28,41 @@
             Route::post('/{id}/toggle-visiable', 'UserController@toggleVisiable')->name('master.post.user.toggle-visiable');
             Route::delete('/{id}/', 'UserController@delete')->name('master.delete.user.delete');
         });
+
+        Route::group(['prefix' => 'managerment'], function(){
+            Route::get('/', 'ManagerController@index')->name('master.get.manager.list-managers');
+            // Route::get('/guest', 'ManagerController@guest')->name('master.get.user.list-guest');
+            // Route::get('/create', 'ManagerController@create')->name('master.get.user.create');
+            // Route::post('/save-create', 'ManagerController@saveCreate')->name('master.post.user.save-create');
+            // Route::get('/{id}/edit/', 'ManagerController@edit')->name('master.get.user.edit');
+            // Route::post('/{id}/save-edit/', 'ManagerController@saveEdit')->name('master.post.user.save-edit');
+            // Route::post('/{id}/send-email-verify/', 'ManagerController@sendEmailVerify')->name('master.post.user.send-email-verify');
+            // Route::post('/{id}/toggle-visiable', 'ManagerController@toggleVisiable')->name('master.post.user.toggle-visiable');
+            // Route::delete('/{id}/', 'ManagerController@delete')->name('master.delete.user.delete');
+        });
+
         Route::group(['prefix' => 'contact'], function(){
             Route::get('/', 'ContactController@index')->name('master.get.contact.list-contacts');
             Route::post('/{id}/toggle-visiable', 'ContactController@toggleVisiable')->name('master.delete.contact.toggle-visiable');
             Route::delete('/{id}/', 'ContactController@delete')->name('master.delete.contact.delete');
             Route::get('/{id}/', 'ContactController@detail')->name('master.get.contact.contact-detail');
+        });
+
+        Route::group(['prefix' => 'setting'], function(){
+            Route::get('/', 'SettingController@index')->name('master.get.setting');
+            Route::post('/save-videoIntro', 'ProfileController@saveVideoIntro');
+            Route::post('/delete-videoIntro', 'ProfileController@deleteVideoIntro');
+        });
+
+        Route::group(['prefix' => 'galary'], function(){
+            Route::get('/', 'GallaryController@index')->name('master.get.galary-popular.list-galary-popular');
+            // Route::get('/guest', 'ManagerController@guest')->name('master.get.user.list-guest');
+            // Route::get('/create', 'ManagerController@create')->name('master.get.user.create');
+            // Route::post('/save-create', 'ManagerController@saveCreate')->name('master.post.user.save-create');
+            // Route::get('/{id}/edit/', 'ManagerController@edit')->name('master.get.user.edit');
+            // Route::post('/{id}/save-edit/', 'ManagerController@saveEdit')->name('master.post.user.save-edit');
+            // Route::post('/{id}/send-email-verify/', 'ManagerController@sendEmailVerify')->name('master.post.user.send-email-verify');
+            // Route::post('/{id}/toggle-visiable', 'ManagerController@toggleVisiable')->name('master.post.user.toggle-visiable');
+            // Route::delete('/{id}/', 'ManagerController@delete')->name('master.delete.user.delete');
         });
     });
